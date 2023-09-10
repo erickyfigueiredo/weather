eb260766d07a092e495524cd361ff0094cc0ee50
ericky <erickyfigueiredo@gmail.com>
2023-09-10 18:53:52 +0900

    fix: enviroments secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'yahoo-weather5.p.rapidapi.com',
     XRapidAPIKeyHostHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHostHeaderValue: '0e0d533f1dmsh95a91c0d3f6ccd5p1fc236jsn1b060debd4de'
+    XRapidAPIKeyHostHeaderValue: 'your-header-value-here'
   };
   
